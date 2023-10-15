e5e4a31b10b4010d085d312a942157c83b65850a
kavya-golamaru <kavyagolamaru@gmail.com>
2023-10-15 04:23:01 -0400

    final version

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,5 +1,6 @@
-import React from "react";
 import './styles.css';
+import Modal from './Modal';
+import React, { useState } from 'react';
 const classNames = "large-text block-text";
 const centerTextStyles = {
   display: 'flex',
@@ -8,19 +9,36 @@ const centerTextStyles = {
   left: '575px',     // Vertically center
   height: '100vh',         // Adjust the height as needed
 };
+
 const Welcome = () => {
-    //write functions to make variables that will be passed into game component '
-   const photo = () => {
-    //set photo
-   }
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
   return (
     <div className="image-background">
       <div style={centerTextStyles}>
         <p className={classNames}>GeoTech-er</p>
         <div>
-          <a href="/play">
+        <button onClick={openModal} className="button-container2">Instructions</button>
+        <a href="/play">
             <button className="button-container">Click to Play!</button>
           </a>
+        </div>
+        <div>
+          
+          <div>
+            
+            <Modal isOpen={isModalOpen} onClose={closeModal}>
+              <h2>Instructions</h2>
+              <p>For every image, try to guess its location on the map by clicking on the exact map location. At the end, the score displayes is the total number of meters you were off by in your guesses.</p>
+              <button onClick={closeModal}>Close</button>
+            </Modal>
+          </div>
         </div>
       </div>
     </div>
