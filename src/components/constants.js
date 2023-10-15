526b37627e6c860e7e9500afc089404f87df3264
kavya-golamaru <kavyagolamaru@gmail.com>
2023-10-14 21:55:52 -0400

    stylization changes to welcome page

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,11 +1,28 @@
 import React from "react";
-import image1 from "../img/tech1.jpg";
-
-const IMAGES = [
+import tech1 from "../img/tech1.jpg"
+import tech2 from "../img/tech2.jpg"
+import tech3 from "../img/tech3.jpg"
+import tech4 from "../img/tech4.jpg"
+    const IMAGES = [
         {
-        id: 1,
-        src: image1,
-        location: "tech tower"
+            id: 1,
+            src: tech1,
+            location: "tech tower"
+        },
+        {
+            id: 2,
+            src: tech2,
+            location: "skyline"
+        },
+        {
+            id: 3,
+            src: tech3,
+            location: "student center"
+        },
+        {
+            id: 4,
+            src: tech4,
+            location: "tech square"
         }
     ];
 
