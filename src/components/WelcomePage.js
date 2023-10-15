dc5275cad32fa83028e5b4107226add0be6131c6
kavya-golamaru <kavyagolamaru@gmail.com>
2023-10-14 21:50:14 -0400

    made style changes to welcome page

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,15 +1,23 @@
-
+import React from "react";
+import './styles.css';
+const classNames = "large-text block-text";
+const centerTextStyles = {
+  display: 'flex',
+  justifyContent: 'center', // Horizontally center
+  alignItems: 'center',     // Vertically center
+  height: '100vh',         // Adjust the height as needed
+};
 const Welcome = () => {
     //write functions to make variables that will be passed into game component '
    const photo = () => {
     //set photo
    }
   return (
-    <div>
-      <p>Welcome</p>
+    <div style={centerTextStyles}>
+      <p className={classNames}>GeoGuesser</p>
       <div>
         <a href="/Start">
-          <button>Play</button>
+          <button className="button-container">Click to Play!</button>
         </a>
       </div>
     </div>
